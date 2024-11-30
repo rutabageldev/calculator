70f6beb05913dd88d6a70bdbbae4d62a652f6d77
rutabageldev <187210498+rutabageldev@users.noreply.github.com>
2024-11-30 12:57:40 -0500

    Initiated 'buildCalculator' and roughed out the display

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const numberContainer =document.querySelector(".number-container");
 const topRowContainer =document.querySelector(".top-container");
 const bottomRowContainer =document.querySelector(".bottom-container");
+const calculatorContainer = document.querySelector('.calculator-container');
 
 
 
-populateButtons();
+buildCalculator();
 
 
 
+function buildCalculator() {
+    buildDisplay();
+    buildNumberButtons();
+}
 
-function populateButtons() {
+function buildNumberButtons() {
     for (let i = 9; i > 0; i--) {    
         const numberButton = document.createElement("button");
         numberButton.classList.add("number-button");
@@ -18,4 +23,12 @@ function populateButtons() {
     }
 
     let headerButtons
+}
+
+function buildDisplay() {
+    const calcDisplay = document.createElement('div');
+    calcDisplay.classList.add('calculator-display');
+    calcDisplay.textContent = '0';
+
+    calculatorContainer.prepend(calcDisplay);
 }
