ede8e397bace37da1b5519798721c5000e08c024
rutabageldev <187210498+rutabageldev@users.noreply.github.com>
2024-11-30 19:07:12 -0500

    Basic UI implementation & number input Created functions to dynamically generate number buttons, created calculator object, and implemented functionality to update calculator display with number values.

--- script.js
+++ script.js
@@ -1,34 +1,94 @@
-const numberContainer =document.querySelector(".number-container");
-const topRowContainer =document.querySelector(".top-container");
-const bottomRowContainer =document.querySelector(".bottom-container");
+const numberContainer = document.querySelector(".number-container");
+const topRowContainer = document.querySelector(".top-container");
+const bottomRowContainer = document.querySelector(".bottom-container");
 const calculatorContainer = document.querySelector('.calculator-container');
 
+const calculator = {
+    state: 'idle', //can be idle, add, subtract, multiply, or divide
+    value: '0', //value displayed on calculator display
+}
 
 
 buildCalculator();
+const calculatorDisplay = document.querySelector('.calculator-display');
 
 
+function attachButtonListeners() {
+    const numberButtons = document.querySelectorAll('.number-button');
+    numberButtons.forEach(button => {
+        button.addEventListener("click", (e) => handleInput(e));
+    });
+}
 
 function buildCalculator() {
     buildDisplay();
     buildNumberButtons();
+    attachButtonListeners();
 }
 
 function buildNumberButtons() {
-    for (let i = 9; i > 0; i--) {    
+    for (let i = 9; i > -3; i--) {    
         const numberButton = document.createElement("button");
         numberButton.classList.add("number-button");
-        numberButton.textContent = i;
+        if (i > 0) {
+            numberButton.textContent = i;
+            numberButton.setAttribute('button-type', 'NUMBER');
+        } else {
+            switch (i) {
+                case (0):
+                    numberButton.textContent = ".";
+                    numberButton.setAttribute('button-type', 'OPERATOR');
+                    break;
+                case (-1):
+                    numberButton.textContent = 0;
+                    numberButton.setAttribute('button-type', 'NUMBER');
+                    break;
+                case (-2):
+                    numberButton.textContent="+/-";
+                    numberButton.setAttribute('button-type', 'OPERATOR');
+                    break;
+            }
+        }
         numberContainer.appendChild(numberButton);
     }
-
-    let headerButtons
 }
 
 function buildDisplay() {
     const calcDisplay = document.createElement('div');
     calcDisplay.classList.add('calculator-display');
-    calcDisplay.textContent = '0';
+    calcDisplay.textContent = ' ';
 
     calculatorContainer.prepend(calcDisplay);
+}
+
+function handleInput(e) {
+    let buttonValue = getButtonValue(e);
+    let buttonType = getButtonType(e);
+
+    switch (buttonType) {
+        case ('NUMBER'):
+            if (calculator.value == '0' || isNaN(parseInt(calculator.value))) {
+                calculator.value = buttonValue;
+            } else {
+                calculator.value = calculator.value + buttonValue
+            }
+            break;
+        case ('OPERATOR'):
+            console.log('operator!');
+            break;
+        case ('CLEAR'):
+            console.log('clear!');
+            calculator.value = 0;
+        break;
+    }
+    calculatorDisplay.textContent = calculator.value;
+}
+
+function getButtonType(e) {
+    return e.target.getAttribute('button-type');
+}
+
+function getButtonValue(e) {
+    console.log("User Selection: " + e.target.textContent);
+    return e.target.textContent;
 }
