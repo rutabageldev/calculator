d750bc25d4280f9a7cb302dff7e61e6378643e8f
rutabageldev <187210498+rutabageldev@users.noreply.github.com>
2024-12-01 17:02:10 -0500

    Implemented debug mode

--- script.js
+++ script.js
@@ -19,25 +19,25 @@ logCalculatorState();
 const calculatorDisplay = document.querySelector('.calculator-display');
 
 function attachButtonListeners() {
-    console.log(`Attaching listeners to buttons...`);
+    if(isDebugMode) console.log(`Attaching listeners to buttons...`);
     const numberButtons = document.querySelectorAll('button');
     numberButtons.forEach(button => {
         button.addEventListener("click", (e) => handleInput(e));
     });
-    console.log(`Listeners attached successfully.`);
+    if(isDebugMode) console.log(`Listeners attached successfully.`);
 }
 
 function buildCalculator() {
-    console.log('Building the calculator...');
+    if(isDebugMode) console.log('Building the calculator...');
     buildDisplay();
     buildNumberButtons();
     buildOperatorButtons();
     attachButtonListeners();
-    console.log('Calculator built successfully.');
+    if(isDebugMode) console.log('Calculator built successfully.');
 }
 
 function buildNumberButtons() {
-    console.log(`Building number buttons...`);
+    if(isDebugMode) console.log(`Building number buttons...`);
     const numberButtons = [
         {symbol: 9, type : 'NUMBER'},
         {symbol: 8, type : 'NUMBER'},
@@ -60,21 +60,21 @@ function buildNumberButtons() {
         numberButton.textContent = number.symbol;
         numberContainer.appendChild(numberButton);
     });
-    console.log(`Number buttons built successfully.`);
+    if(isDebugMode) console.log(`Number buttons built successfully.`);
 }
 
 function buildDisplay() {
-    console.log(`Building calculator display...`);
+    if(isDebugMode)  console.log(`Building calculator display...`);
     const calcDisplay = document.createElement('div');
     calcDisplay.classList.add('calculator-display');
     calcDisplay.textContent = '0';
 
     calculatorContainer.prepend(calcDisplay);
-    console.log(`Calculator display built successfully.`);
+    if(isDebugMode) console.log(`Calculator display built successfully.`);
 }
 
 function buildOperatorButtons(){
-    console.log(`Building operator buttons...`);
+    if(isDebugMode) console.log(`Building operator buttons...`);
     const operators = [
         {symbol: '+', type: 'ADD'},
         {symbol: '-', type: 'SUBTRACT'},
@@ -88,24 +88,24 @@ function buildOperatorButtons(){
         operatorButton.textContent = operator.symbol;
         operatorContainer.appendChild(operatorButton);
     })
-    console.log(`Operator buttons built successfully...`);
+    if(isDebugMode) console.log(`Operator buttons built successfully...`);
 }
 
 function handleInput(e) {
     let buttonValue = getButtonValue(e);
     let buttonType = getButtonType(e);
 
-    console.log(`Button clicked: ${buttonValue} (Type: ${buttonType})`);
+    if(isDebugMode) console.log(`Button clicked: ${buttonValue} (Type: ${buttonType})`);
 
     switch (buttonType) {
         case ('NUMBER'):
-            console.log(`Calculator value before update: ${calculator.value}`);
+            if(isDebugMode) console.log(`Calculator value before update: ${calculator.value}`);
             if (calculator.value == '0' || isNaN(parseInt(calculator.value))) {
                 calculator.value = buttonValue;
             } else {
                 calculator.value = calculator.value + buttonValue
             }
-            console.log(`Updated calculator value: ${calculator.value}`);
+            if(isDebugMode) console.log(`Updated calculator value: ${calculator.value}`);
             break;
         case ('ADD'):
         case ('CHANGE-SIGN'):
@@ -113,14 +113,14 @@ function handleInput(e) {
         case ('DIVIDE'):
         case ('MULTIPLY'):
         case ('SUBTRACT'):    
-            console.log(`Operator selected: ${buttonType}`);
+        if(isDebugMode) console.log(`Operator selected: ${buttonType}`);
             break;
         case ('CLEAR'):
-            console.log(`Clearing calculator value`);
+            if(isDebugMode) console.log(`Clearing calculator value`);
             calculator.value = 0;
             break;
         default: 
-            console.log(`Unhandled button type: ${buttonType}`);
+        if(isDebugMode) console.log(`Unhandled button type: ${buttonType}`);
             break;
 
     }
